0d1e1a30f39591a7b85439337066b8214264f32b
odococo <ivan_lamperti@outlook.it>
2017-09-20 14:21:16 +0200

    niente webhook

--- 333089594.py
+++ 333089594.py
@@ -44,8 +44,7 @@ def error(bot, update, error):
 
 def main():
     # Create the EventHandler and pass it your bot's token.
-    ID = "333089594"
-    TOKEN = ID + ":AAFossfi9mGnY648Eb5mv3wKO0NbHedrXq0"
+    TOKEN = "333089594:AAFossfi9mGnY648Eb5mv3wKO0NbHedrXq0"
     PORT = int(os.environ.get('PORT', '5000'))
     
     updater = Updater(TOKEN)
@@ -64,11 +63,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    #updater.start_polling()
-    updater.start_webhook(listen = "0.0.0.0",
-                      port = PORT,
-                      url_path = TOKEN)
-    updater.bot.setWebhook("https://fancazzistibot.herokuapp.com/" + ID + ".py")
+    updater.start_polling()
+    #updater.start_webhook(listen = "0.0.0.0",
+     #                 port = PORT,
+      #                url_path = TOKEN)
+    #updater.bot.setWebhook("https://<appname>.herokuapp.com/" + TOKEN")
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
