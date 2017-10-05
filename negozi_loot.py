7d08846b89bdfbdd00feea49027ff2617739f0a9
odococo <ivan_lamperti@outlook.it>
2017-10-05 22:38:42 +0200

    Update negozi_loot.py

--- negozi_loot.py
+++ negozi_loot.py
@@ -1,104 +1,66 @@
-import json
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import json
 import utils
+import re
+
+API = "http://fenixweb.net:3300/api/v2/"
+TOKEN = "cMeBZ7H22h8ApDho1722"
+ITEMS = API + TOKEN + "/items"
+SHOP = API + TOKEN + "/shop/"
 
 NEGOZI = (
-    79297374614,
-    53930040696,
-    49323534240,
-    91235949067,
-    52769617366,
-    97456485241,
-    98128917950,
-    33993865643,
-    54776792608,
-    25434498998,
-    36908597775,
-    23578216146,
+    14501401327,
+    18164013683,
+    26214958953,
+    27710823262,
+    31437794971,
+    36268311737,
+    37065414168,
+    45796402190,
+    50778334988,
+    55605058279,
+    68873282471,
+    71938314999,
+    82265422713,
+    84499506942,
+    90104392404,
+    97210686305,
 )
 
-def create_shops(text):
-    oggetti = get_items(text)
-    negozi = ""
-    while oggetti:
-        negozi += "`" + create_shop(oggetti[:10]) + "`\n"
-        oggetti = oggetti[10:]
-    return negozi
-    
-def create_shop(items):
-    shop = "/negozio "
-    shop += ",".join(list(map(lambda item: (
-        item['oggetto'] + ":" +
-        str(item['prezzo']) + ":" +
-        str(item['necessari'])), items)))
-    return shop
+OGGETTI = {}
+OGGETTI_DB = {}
 
-def get_items(text):
-    oggetti = text.split("\n")
-    items = {}
-    for oggetto in oggetti:
-        if oggetto.startswith(">"):
-            item = get_item(oggetto)
-            items[get_db_item(item['oggetto'])['id']] = item
-    for negozio in NEGOZI:
-        shop = get_negozio(negozio)
-        for item in shop:
-            if item['item_id'] in items:
-                items[item['item_id']]['prezzo'] = item['price']
-    return list(items.values())
-    
-def get_item(text):
-    param = text.split(" ")
-    item = {
-        'necessari': int(param[1]),
-        'oggetto': " ".join(param[5:-1]),
+def set_items():
+    global OGGETTI 
+    global OGGETTI_DB 
+    oggetti = utils.get_content(ITEMS, True)['res']
+    OGGETTI = {oggetto['name']: get_item(oggetto) for oggetto in oggetti}
+    OGGETTI_DB = {oggetto['id']: oggetto['name'] for oggetto in oggetti}
+  
+def get_item(oggetto):
+    return {
+        'stima': oggetto['estimate'],
+        'id': oggetto['id'],
         'prezzo': 0
     }
-    return item
+  
+def set_prices():
+    negozi = [utils.get_content(SHOP + str(codice), True)['res'] for codice in NEGOZI]
+    global OGGETTI
+    for negozio in negozi:
+        for oggetto in negozio:
+            OGGETTI[OGGETTI_DB[oggetto['item_id']]]['prezzo'] = oggetto['price']
     
-def get_db_item(nome_oggetto):
-    url = "http://fenixweb.net:3300/api/v1/items/" + nome_oggetto
-    items = utils.get_content(url, parse_json=True)['res']
-    if isinstance(items, list):
-        for item in items:
-            if item['name'] == nome_oggetto:
-                return item
-    return items
-
-def get_negozio(codice):
-    url = "http://fenixweb.net:3300/api/v1/shop/" + str(codice)
-    return utils.get_content(url, parse_json=True)['res']
-    
-if __name__ == "__main__":
-    print(create_shops("""Lista oggetti necessari per Corazza Antimateria: 
-> 2 su 2 di Ala della Fenice (E)
-> 1 su 2 di Lama Celeste (E)
-> 1 su 1 di Manico dello Stregone (E)
-> 1 su 1 di Scheggia di Hatrurite (E)
-> 1 su 3 di Blocco della Fine (L)
-> 2 su 4 di Innesto (L)
-> 3 su 4 di Lacrima Galattica (L)
-> 1 su 1 di Manico della Fine (L)
-> 4 su 4 di Oro Bianco (L)
-> 1 su 1 di Oro Nero (L)
-> 1 su 1 di Plutonio (L)
-> 2 su 4 di Polvere Divina (L)
-> 3 su 4 di Ambra Nera (UR)
-> 7 su 10 di Avorio (UR)
-> 4 su 5 di Laccio Elastico (UR)
-> 5 su 8 di Laccio Statico (UR)
-> 1 su 2 di Pietra Rituale (UR)
-> 1 su 3 di Scaglia di Adamantio (UR)
-> 4 su 5 di Scalpello (UR)
-> 4 su 5 di Carbonio (R)
-> 7 su 8 di Catalizzatore (R)
-> 1 su 3 di Cristallo Opaco (R)
-> 2 su 3 di Diamante (R)
-> 4 su 5 di Pietra del Sangue (R)
-> 1 su 7 di Rivestimento da Caccia (R)
-> 1 su 2 di Runa Instabile (R)
-> 1 su 1 di Runa Stabile (R)
-> 2 su 9 di Unguento (R)
-> 6 su 9 di Laccio (NC)
-> 2 su 6 di Resina (NC)
-> 1 su 7 di Telo (NC)"""))
+def value(text):
+    oggetti = [oggetto.split(" ") for oggetto in text.split("\n") if re.match("^[0-9]", oggetto)]
+    prezzo_craft = int(text[text.rindex(":")+1:text.index("§")].replace("'", ""))
+    oggetti = {" ".join(oggetto[2:-1]): int(oggetto[0]) for oggetto in oggetti}
+    text = "Prezzo bot: {}\n".format(sum([OGGETTI[oggetto]['stima']*oggetti[oggetto] for oggetto in oggetti]))
+    prezzi_mancanti = [oggetto for oggetto in oggetti if OGGETTI[oggetto]['prezzo'] == 0]
+    text += "Prezzo negozi: {}\n".format(sum([OGGETTI[oggetto]['prezzo']*oggetti[oggetto] for oggetto in oggetti]))
+    if prezzi_mancanti:
+        text += "Prezzi mancanti: {}\n".format("\n".join(prezzi_mancanti))
+    text += "Prezzo craft: {}".format(prezzo_craft)
+    return text
