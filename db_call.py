50e830ab0b6d9a397da90568b33c4e7db3f5f015
odococo <ivan_lamperti@outlook.it>
2017-11-17 21:51:30 +0100

    Update db_call.py

--- db_call.py
+++ db_call.py
@@ -11,7 +11,7 @@ from urllib import parse
 import psycopg2
 import psycopg2.extras
 
-from utils import is_numeric
+import utils
 
 
 # Enable logging
@@ -41,9 +41,9 @@ def add_bot(bot):
     add_user(bot)
     
 def get_user(key_value):
-    key_value = (str(key_value) if is_numeric(key_value, True)
+    key_value = (str(key_value) if utils.is_numeric(key_value, True)
                  else key_value)
-    if is_numeric(key_value):
+    if utils.is_numeric(key_value):
         query = TABELLE['users']['select']['from_id']
     else:
         query = TABELLE['users']['select']['from_username']
