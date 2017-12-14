25714756a219da132af5c9a51ed8ac3cdb19d464
Nicolo <nicofirst1@gmail.com>
2017-12-14 21:26:28 +0100

    bug fixed

--- bot_classes.py
+++ bot_classes.py
@@ -25,7 +25,7 @@ class Loot:
             ricerca_decor = db.elegible_loot_user(self.ricerca)
             stima_decor = db.elegible_loot_user(self.stima)
             dispatcher.add_handler(RegexHandler("^Lista oggetti necessari per", ricerca_decor, pass_user_data=True))
-            dispatcher.add_handler(MessageHandler(Filters.text, stima_decor, pass_user_data=True))
+            #dispatcher.add_handler(MessageHandler(Filters.text, stima_decor, pass_user_data=True))
 
 
 
