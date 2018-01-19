22c29c3686b84113b6535fa7c7fcc663cb40bb0f
dizzi <nicofirst1@gmail.com>
2018-01-19 14:45:35 +0100

    bug fixed

--- Other/track_activity.py
+++ Other/track_activity.py
@@ -851,8 +851,8 @@ In questa sezione puoi visualizzare informazioni varie 📊 tra cui:
             to_send="Complimenti! Hai guadagnato un punto, sei arrivato a "+str(punteggio+1)+" punti"
             self.db.update_activity_points(job.context['user_id'],1)
         else:
-            to_send = "Purtroppo non hai fatto in tempo a rispondere a tutti i messaggi....perdi un punto\nSei arrivato a " + str(punteggio - 1) + " punti"
-            self.db.update_activity_points(job.context['user_id'], -1)
+            to_send = "Purtroppo non hai fatto in tempo a rispondere a tutti i messaggi...."
+            #self.db.update_activity_points(job.context['user_id'], -1)
 
         bot.sendMessage(job.context['chat_id'],to_send)
 
