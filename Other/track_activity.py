bffe42454d02492258797ea9c6a5db9e60b04afe
dizzi <nicofirst1@gmail.com>
2018-01-19 09:03:29 +0100

    bug fixed

--- Other/track_activity.py
+++ Other/track_activity.py
@@ -547,9 +547,9 @@ In questa sezione puoi visualizzare informazioni varie 📊 tra cui:
 
         for elem in counter.keys():
             if elem<10:
-                to_send += "<b>" + str(elem) + ":00</b> " + self.filler(tot, counter[elem]*2.4) + "\n"
+                to_send += " <b>" + str(elem) + ":00</b> " + self.filler(tot, counter[elem]*2.4) + "\n"
             else:
-                to_send += " <b>" + str(elem) + ":00 </b> " + self.filler(tot, counter[elem]*2.4) + "\n"
+                to_send += "<b>" + str(elem) + ":00</b> " + self.filler(tot, counter[elem]*2.4) + "\n"
 
         return to_send
 
