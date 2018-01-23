4cc0908e982867768409b417632dc27334a54e8e
dizzi <nicofirst1@gmail.com>
2018-01-23 13:05:41 +0100

    bug fixed

--- Other/track_activity.py
+++ Other/track_activity.py
@@ -43,6 +43,9 @@ from telegram.ext import BaseFilter, MessageHandler, CommandHandler, CallbackQue
 #           update.message.date
 #     )
 #
+from Sentiment_analisys.Analyzer import Analyzer
+
+
 class Track:
     """Classe per il track delle activity sul gruppo dei Fancazzisti"""
 
@@ -158,6 +161,13 @@ In questa sezione puoi visualizzare informazioni varie 📊 tra cui:
         disp.add_handler(CallbackQueryHandler(self.activity_user, pattern="/activity_user", pass_user_data=True))
         disp.add_handler(CallbackQueryHandler(self.activity_altro, pattern="/activity_altro", pass_user_data=True))
         disp.add_handler(CallbackQueryHandler(self.classify, pattern="/activity_sentiment", pass_chat_data=True, ))
+
+        classified = self.get_activity_by("all")
+        classified=[{'text':elem['text'],'sentiment':elem['sentiment']} for elem in classified if isinstance(elem['sentiment'], int)]
+        labled=[elem for elem in classified if elem['sentiment']!=0]
+        unlabled=[elem for elem in classified if elem['sentiment']==0]
+        self.analyzer=Analyzer(labled,unlabled)
+        self.analyzer.train_models()
 
 
 
